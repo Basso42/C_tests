b4a517ea3c4a33ed2cbf97a2a6652c3daa151536
gamer35 <florian.basso@ensae.fr>
2023-11-07 01:36:52 +0000

    first steps

---
+++ pun.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h> 
+int main(void) //main script
+{
+printf("To C, or not to C: that is the question.\n");
+return 0;
+}
+
+
+
+// You first write statements (library and main)
+//inside the brackets are statements
+
+// c compiler in sspcloud is cc or gcc;  cc -o pun pun.c
+// and then ./pun to execute it
